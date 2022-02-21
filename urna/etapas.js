d4d9c1b67a32f642ec10036fddce8b477e9d4a67
albegehtv <63756587+albegehtv@users.noreply.github.com>
2022-02-21 15:50:23 -0300

    Add files via upload

---
+++ urna/etapas.js
@@ -0,0 +1,58 @@
+let etapas = 
+[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: 
+        [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABV',
+                fotos:
+                [
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            
+            {
+                numero: '38151',
+                nome: 'Fuladno de Tal',
+                partido: 'ABdV',
+                fotos:
+                [
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ] 
+            },
+        ]
+    },
+
+    {
+        titulo:'PREFEITO',
+        numeros: 2,
+        candidatos: 
+        [
+            {
+                numero: '95',
+                nome: 'Fulano de Tal',
+                partido: 'ANR',
+                fotos:
+                [
+                    {url:'3311.jpg', legenda:'Prefeito'},
+                    {url:'38611.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            
+            {
+                numero: '38',
+                nome: 'Sultano',
+                partido: 'pDKR',
+                fotos:
+                [
+                    {url:'38161.jpg', legenda:'Prefeito'},
+                    {url:'38441.jpg', legenda: 'Vice-Prefeito', small:true}
+                ] 
+            },
+        ]
+    }
+ ];
